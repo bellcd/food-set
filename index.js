063b81b1017718546bd094dc9163e92dbe088240
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-07-30 18:17:30 -0400

    Upgrades mouseover event on area

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 $(window).on('load', function() {
   let $mapContainer = $('.map-container');
+  let $close, $temp;
 
   let $area1 = $('#area1');
   $area1.on('mouseover', function(event) {
     event.preventDefault();
+
+    if ($temp) {
+      removeTemp();
+    }
     console.log(event);
     $(event.target).toggleClass('hello');
-
-    $mapContainer.append('<div id="temp">Here is stuff</div>');
-    // console.log(event.pageY);
-    $('#temp').attr('position', 'absolute');
-    $('#temp').css({
+    $temp = $('<div id="temp">Here is stuff</div>').appendTo($mapContainer);
+    $temp.attr('position', 'absolute');
+    $temp.css({
       'top': event.pageY + 'px',
       'left': event.pageX + 'px'
     });
+
+    $close = $('<p>close</p>').appendTo($temp);
+    $close.css({
+      cursor: 'pointer'
+    })
+
+    $close.on('click', removeTemp);
   });
+
+  const removeTemp = function() {
+    $temp.remove();
+  }
 });

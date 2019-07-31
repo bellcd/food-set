ea118c62f517da5b051501a1f7b59ca3dd627832
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-07-31 00:18:03 -0400

    Improves pop up functionality

--- index.js
+++ index.js
@@ -2,28 +2,30 @@ $(window).on('load', function() {
   let $mapContainer = $('.map-container');
   let $close, $temp;
 
-  let $area1 = $('#area1');
-  $area1.on('mouseover', function(event) {
-    event.preventDefault();
+  let spiceList = [$('#chili'), $('#turmeric'), $('#curry'), $('#saffron'), $('#cinnamon'), $('#bay-leaves'), $('#peppercorn1'), $('#peppercorn2')];
+  spiceList.forEach(spiceArea => {
+    spiceArea.on('mouseover', function(event) {
+      event.preventDefault();
 
-    if ($temp) {
-      removeTemp();
-    }
-    console.log(event);
-    $(event.target).toggleClass('hello');
-    $temp = $('<div id="temp">Here is stuff</div>').appendTo($mapContainer);
-    $temp.attr('position', 'absolute');
-    $temp.css({
-      'top': event.pageY + 'px',
-      'left': event.pageX + 'px'
+      if ($temp) {
+        removeTemp();
+      }
+      console.log(event);
+      $(event.target).toggleClass('hello');
+      $temp = $('<div id="temp">Here is stuff</div>').appendTo($mapContainer);
+      $temp.attr('position', 'absolute');
+      $temp.css({
+        'top': event.pageY + 'px',
+        'left': event.pageX + 'px'
+      });
+
+      $close = $('<p>close</p>').appendTo($temp);
+      $close.css({
+        cursor: 'pointer'
+      })
+
+      $close.on('click', removeTemp);
     });
-
-    $close = $('<p>close</p>').appendTo($temp);
-    $close.css({
-      cursor: 'pointer'
-    })
-
-    $close.on('click', removeTemp);
   });
 
   const removeTemp = function() {

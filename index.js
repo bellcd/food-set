9c4fabbe2aa60a9df9dd14ea415e5e60f043e447
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-07-31 01:17:28 -0400

    Polishes presentation

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 $(window).on('load', function() {
   let $mapContainer = $('.map-container');
   let $close, $temp;
+  let spiceList = $('area');
+  const spiceLinks = [
 
-  let spiceList = [$('#chili'), $('#turmeric'), $('#curry'), $('#saffron'), $('#cinnamon'), $('#bay-leaves'), $('#peppercorn1'), $('#peppercorn2')];
-  spiceList.forEach(spiceArea => {
-    spiceArea.on('mouseover', function(event) {
+  ];
+  for (let i = 0; i < spiceList.length; i++) {
+    $(spiceList[i]).on('mouseover', function(event) {
       event.preventDefault();
 
       if ($temp) {
         removeTemp();
       }
-      console.log(event);
-      $(event.target).toggleClass('hello');
-      $temp = $('<div id="temp">Here is stuff</div>').appendTo($mapContainer);
+
+      $temp = $('<div id="temp"></div>').appendTo($mapContainer);
+      const name1 = event.target.id;
+      const name2 = event.target.classList[0];
+
+      const $tempTitle = $(`<a href="${event.target.href}"><h3>${name2 === undefined ? name1 : name2}</h3></a>`).appendTo($temp);
+      $tempTitle.addClass('temp-title');
+
       $temp.attr('position', 'absolute');
       $temp.css({
         'top': event.pageY + 'px',
         'left': event.pageX + 'px'
       });
+
+      $temp.addClass()
 
       $close = $('<p>close</p>').appendTo($temp);
       $close.css({
@@ -26,7 +35,7 @@ $(window).on('load', function() {
 
       $close.on('click', removeTemp);
     });
-  });
+  }
 
   const removeTemp = function() {
     $temp.remove();
